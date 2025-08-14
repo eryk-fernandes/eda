f555b753a8091e883eaa4b3e8e3bc251f2a82fd5
Eryk Fernandes <erykfernandespro@gmail.com>
2025-08-14 12:59:23 -0300

    Push na pilha

--- src/pilha/PilhaLigada.java
+++ src/pilha/PilhaLigada.java
@@ -7,9 +7,21 @@ public class PilhaLigada {
     public void push(int dado) {
 
         No no = new No(dado);
+
+        if (isVazio()) {
+            topo = no;
+            return;
+        }
+
+        no.setProximo(topo);
+        topo = no;
     }
 
     public void pop() {
 
+    }
+
+    public boolean isVazio() {
+        return topo == null;
     }
 }
